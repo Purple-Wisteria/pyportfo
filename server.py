7fdb685d2f43a5470a01891f210be63743276fd5
Raj <ssraj74@gmail.com>
2020-07-02 08:24:23 -0400

    python project

---
+++ server.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, url_for, request, redirect
+import csv
+
+app = Flask(__name__)
+print(__name__)
+
+
+@app.route('/<string:page_name>')
+def works(page_name):
+    return render_template(page_name)
+
+
+"""
+@app.route('/index.html')
+def render_home():
+    return render_template('index.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+@app.route('/works.html')
+def works():
+    return render_template('works.html')
+"""
+
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        name = data["name"]
+        email = data["email"]
+        message = data["message"]
+        file = database.write(f'\n{name},{email},{message}')
+
+
+def write_to_csv(data):
+    with open('database.csv', newline='', mode='a') as database2:
+        name = data["name"]
+        email = data["email"]
+        message = data["message"]
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([name, email, message])
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'Record is not saved to database'
+    else:
+        return 'something went wrong'
